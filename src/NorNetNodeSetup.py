145b6d9156d72755cb6b3a7884af75e29c889f6e
Thomas Dreibholz <dreibh@simula.no>
2012-08-10 11:22:14 +0200

    Local network configuration is only set/unset on start/stop, not when certain providers come up/down.

--- src/NorNetNodeSetup.py
+++ src/NorNetNodeSetup.py
@@ -417,7 +417,7 @@ def _makeTunnelboxNetwork(outputFile, state, localInterface,
       action = 'Setting up'
    elif (state == 'stop'):
       action = 'Tearing down'
-   outputFile.write('   log-action "' + action + ' local networks for ' + \
+   outputFile.write('      log-action "' + action + ' local networks for ' + \
                     localProvider['provider_long_name'] + ' ..."\n')
 
    for version in [ 4, 6 ]:
@@ -426,11 +426,11 @@ def _makeTunnelboxNetwork(outputFile, state, localInterface,
 
       localAddress = makeNorNetIP(localProviderIndex, localSiteIndex, NorNet_NodeIndex_Tunnelbox, -1, version)
       if state == 'start':
-         outputFile.write('   make-address ' + localInterface + ' ' + str(localAddress) + '\n')
+         outputFile.write('      make-address ' + localInterface + ' ' + str(localAddress) + '   && \\\n')
       elif state == 'stop':
-         outputFile.write('   remove-address ' + localInterface + ' ' + str(localAddress) + '\n')
+         outputFile.write('      remove-address ' + localInterface + ' ' + str(localAddress) + '   && \\\n')
 
-   outputFile.write('   log-result $RESULT_GOOD\n')
+   outputFile.write('      log-result $RESULT_GOOD || log-result $RESULT_BAD\n\n')
 
 
 def makeTunnelboxBootstrap(localSiteIndex, localProviderIndex, localAddress, configNamePrefix):
@@ -518,20 +518,21 @@ def makeTunnelBoxConfiguration(fullSiteList, localSite, configNamePrefix, v4only
 
 
    # ====== Remove local setup ==============================================
-   outputFile.write('if [ "$state" = "stop" -o "$state" = "restart" ] ; then\n')
-   outputFile.write('   log-action "Turning off IP forwarding ..."\n')
-   outputFile.write('   sysctl -q net.ipv4.ip_forward=0\n')
-   outputFile.write('   sysctl -q net.ipv6.conf.all.forwarding=0\n')
-   outputFile.write('   log-result $RESULT_GOOD\n')
+   outputFile.write('if [ "$selectedProviders" == "" ] ; then\n')
+   outputFile.write('   if [ "$state" = "stop" -o "$state" = "restart" ] ; then\n')
+   outputFile.write('      log-action "Turning off IP forwarding ..."\n')
+   outputFile.write('      sysctl -q net.ipv4.ip_forward=0   && \\\n')
+   outputFile.write('      sysctl -q net.ipv6.conf.all.forwarding=0   && \\\n')
+   outputFile.write('      log-result $RESULT_GOOD || log-result $RESULT_BAD\n\n')
    for localProviderIndex in localProviderList:
       _makeTunnelboxNetwork(outputFile, 'stop', localInterface,
                             localProviderList[localProviderIndex], localSiteIndex, v4only)
    if localSiteIndex == NorNet_SiteIndex_Central:
-      outputFile.write('   log-action "Turning off IPv4 NAT ..."\n')
-      outputFile.write('   remove-nat ' + str(fullNorNetIPv4) + ' "' + sourceNatRange + '"\n')
-      outputFile.write('   log-result $RESULT_GOOD\n')
+      outputFile.write('      log-action "Turning off IPv4 NAT ..."\n')
+      outputFile.write('      remove-nat ' + str(fullNorNetIPv4) + ' "' + sourceNatRange + '"   && \\\n')
+      outputFile.write('      log-result $RESULT_GOOD || log-result $RESULT_BAD\n')
+   outputFile.write('   fi\n')
    outputFile.write('fi\n\n')
-
 
    # ====== Configure tunnels and routing ===================================
    outputFile.write('availableProviders="')
@@ -569,20 +570,23 @@ def makeTunnelBoxConfiguration(fullSiteList, localSite, configNamePrefix, v4only
 
 
    # ====== Make local setup ================================================
-   outputFile.write('\nif [ "$state" = "start" -o "$state" = "restart" ] ; then\n')
-   outputFile.write('   log-action "Turning on IP forwarding..."\n')
-   outputFile.write('   sysctl -q net.ipv4.ip_forward=1 && \\\n')
-   outputFile.write('   sysctl -q net.ipv6.conf.all.forwarding=1 && \\\n')
-   outputFile.write('   sysctl -q net.ipv4.tcp_ecn=1 && \\\n')
-   outputFile.write('   log-result $RESULT_GOOD || log-result $RESULT_BAD\n')
+   outputFile.write('\nif [ "$selectedProviders" == "" ] ; then\n')
+   outputFile.write('   if [ "$state" = "start" -o "$state" = "restart" ] ; then\n')
+   outputFile.write('      log-action "Turning on IP forwarding..."\n')
+   outputFile.write('      sysctl -q net.ipv4.ip_forward=1 && \\\n')
+   outputFile.write('      sysctl -q net.ipv6.conf.all.forwarding=1 && \\\n')
+   outputFile.write('      sysctl -q net.ipv4.tcp_ecn=1 && \\\n')
+   outputFile.write('      log-result $RESULT_GOOD || log-result $RESULT_BAD\n\n')
+
    for localProviderIndex in localProviderList:
       _makeTunnelboxNetwork(outputFile, 'start', localInterface,
                             localProviderList[localProviderIndex], localSiteIndex, v4only)
    if localSiteIndex == NorNet_SiteIndex_Central:
-      outputFile.write('   log-action "Turning on IPv4 NAT ..."\n')
-      outputFile.write('   make-nat ' + str(fullNorNetIPv4) + ' "' + sourceNatRange + '" && \\n')
-      outputFile.write('   log-result $RESULT_GOOD || log-result $RESULT_BAD\n')
-   outputFile.write('fi\n\n')
+      outputFile.write('      log-action "Turning on IPv4 NAT ..."\n')
+      outputFile.write('      make-nat ' + str(fullNorNetIPv4) + ' "' + sourceNatRange + '" && \\\n')
+      outputFile.write('      log-result $RESULT_GOOD || log-result $RESULT_BAD\n')
+   outputFile.write('   fi\n')
+   outputFile.write('fi\n')
 
 
    outputFile.write('\nif [ "$state" = "stop" -o "$state" = "start" -o "$state" = "restart" ] ; then\n')
