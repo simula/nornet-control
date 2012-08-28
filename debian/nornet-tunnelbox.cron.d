03d14f8b037e55c9495c52ce892c5d905b9183aa
Thomas Dreibholz <dreibh@simula.no>
2012-08-28 14:14:55 +0200

    Cronjob improvements.

--- debian/nornet-tunnelbox.cron.d
+++ debian/nornet-tunnelbox.cron.d
@@ -1,1 +1,1 @@
-3,8,13,18,23,28,33,38,43,48,53,58   root    test -x /etc/init.d/nornet-tunnelbox && /etc/init.d/nornet-tunnelbox configure
+3,8,13,18,23,28,33,38,43,48,53,58 * * * *  root    test -x /etc/init.d/nornet-tunnelbox && /etc/init.d/nornet-tunnelbox configure
