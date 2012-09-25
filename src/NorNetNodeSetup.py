b072c5323787055156c336bfbcec43467ad390e0
Thomas Dreibholz <dreibh@simula.no>
2012-09-25 12:57:46 +0200

    Some tunnelbox configuration handling fixes.

--- src/NorNetNodeSetup.py
+++ src/NorNetNodeSetup.py
@@ -703,6 +703,7 @@ def makeTunnelBoxConfiguration(fullSiteList, localSite, configNamePrefix):
       for localProviderIndex in localProviderList:
          if ( ((onlyDefault == True)  and (localProviderIndex == localSite['site_default_provider_index'])) or \
               ((onlyDefault == False) and (localProviderIndex != localSite['site_default_provider_index'])) ):
+            localProvider = localProviderList[localProviderIndex]
             tbpName = _makeTunnelboxProvider(fullSiteList, localSite,
                                              localProviderList, localProvider,
                                              pathNumber, configNamePrefix)
@@ -710,10 +711,15 @@ def makeTunnelBoxConfiguration(fullSiteList, localSite, configNamePrefix):
 
    outputFile.write('check-providers "$all_providers" "$selectedProviders"\n')
    outputFile.write('tbc_success=1\n')
-   outputFile.write('for provider in $providersOld ; do\n')
+   outputFile.write('for provider in $all_providers ; do\n')
    outputFile.write('   if [[ "$selectedProviders" =~ ^@ALL$|^$provider$$|^$provider$([ ])|([ ])$provider$([ ])|([ ])$provider$$ ]] ; then\n')
-   outputFile.write('      echo "Configuring provider $provider ..."\n')
-   outputFile.write('      . ./tunnelbox-$provider || tbc_success=0\n')
+   outputFile.write('      echo "Configuring provider $provider from tunnelbox-${provider}-config ..."\n')
+   outputFile.write('      if [ -e tunnelbox-${provider}-config ] ; then\n')
+   outputFile.write('         . ./tunnelbox-${provider}-config || tbc_success=0\n')
+   outputFile.write('      else\n')
+   outputFile.write('         echo >&2 "ERROR: tunnelbox-${provider}-config does not exist!"\n')
+   outputFile.write('         tbc_success=0\n')
+   outputFile.write('      fi\n')
    outputFile.write('   else\n')
    outputFile.write('      echo "Skipping provider $provider ..."\n')
    outputFile.write('   fi\n')
