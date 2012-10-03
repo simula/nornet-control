0f211ad8fc71e689e378918ced51d7749932fa0a
Thomas Dreibholz <dreibh@simula.no>
2012-10-03 11:25:10 +0200

    Updated test script.

--- src/tests/Monitor/nagmap/t2.php
+++ src/tests/Monitor/nagmap/t2.php
@@ -18,86 +18,109 @@
 
 function getNorNetStatus() {
   include('config.php');
-  $fp = fopen($nagios_status_dat_file,"r");
-  $type = "";
-  $data = Array();
+  $fp = fopen($nagios_status_dat_file, "r");
+
+  $hostName    = "";
+  $serviceName = "";
+  $entryType   = "";
+  $status      = Array();
   while (!feof($fp)) {
-    $line = trim(fgets($fp));
-    //ignore all commented lines - hop to the next itteration
-    if (empty($line) OR preg_match("/^;/", $line) OR preg_match("/^#/", $line)) {
-      continue;
-    }
-    //if end of definition, skip to next itteration
-    if (preg_match("/}/",$line)) {
-      $type = "0";
-      unset($host);
-      continue;
-    }
-    if (preg_match("/^hoststatus {/", $line)) {
-      $type = "hoststatus";
-    };
-    if (preg_match("/^servicestatus {/", $line)) {
-      $type = "servicestatus";
-    };
-    if(!preg_match("/}/",$line) && ($type == "hoststatus" | $type == "servicestatus")) {
-      $line = trim($line);
-      $pieces = explode("=", $line, 2);
-      //do not bother with invalid data
-      if (count($pieces) < 2) { continue; };
-      $option = trim($pieces[0]);
-      $value  = trim($pieces[1]);
-      if ($option == "host_name") {
-        $host = $value;
-      }
-      else if( ($option != "last_hard_state") &&
-               ($option != "plugin_output") &&
-               ($option != "performance_data") &&
-               ($option != "service_description") &&
-               ($option != "check_command") ) {
-         continue;
-      }
-      echo "Add: H=".$host." T=".$type." -> ".$option."=".$value."<br />\n";
-      if (!isset($data[$host][$type][$option])) {
-        $data[$host][$type][$option] = "";
-      }
+      // ====== Read line from status.dat ===================================
+      $line = trim(fgets($fp));
 
-      if (!isset($data[$host]['servicestatus']['last_hard_state'])) {
-        $data[$host]['servicestatus']['last_hard_state'] = "";
+      // ====== Handle line =================================================
+      if ( (empty($line)) ||
+           (preg_match("/^;/", $line)) ||
+           (preg_match("/^#/", $line)) ) {
+         // Ignore empty line or comment.
       }
+      elseif (preg_match("/}/",$line)) {
+         // End of entry
+         $hostName    = "";
+         $serviceName = "";
+      }
+      elseif (preg_match("/^hoststatus {/", $line)) {
+         $entryType = "hoststatus";
+      }
+      elseif (preg_match("/^servicestatus {/", $line)) {
+         $entryType = "servicestatus";
+      }
+      elseif ( (!preg_match("/}/", $line)) &&
+              (($entryType == "hoststatus") || ($entryType == "servicestatus")) ) {
+         $line   = trim($line);
+         $pieces = explode("=", $line, 2);
+         if (count($pieces) >= 2) {
+            $parameterName = trim($pieces[0]);
+            $value         = trim($pieces[1]);
+            if ($parameterName == "host_name") {
+               $hostName    = $value;
+               $serviceName = "";
+            }
+            elseif ($parameterName == "service_description") {
+               $serviceName = $value;
+            }
+            elseif ( ($parameterName == "last_hard_state") ||
+                     ($parameterName == "plugin_output")  ||
+                     ($parameterName == "performance_data") ||
+                     ($parameterName == "check_command") ) {
+               if (!isset($status[$hostName][$serviceName]['last_hard_state'])) {
+                  // Initialize state with empty string, just for the case it
+                  // is missing in status.dat.
+                  $status[$hostName][$serviceName]['last_hard_state'] = "";
+               }
 
-      if ($option == "last_hard_state") {
-        if ($value >= $data[$host][$type][$option]) {
-          $data[$host][$type][$option] = $value;
-        }
-        if (($data[$host]['hoststatus']['last_hard_state'] == 0) && ($data[$host]['servicestatus']['last_hard_state'] == 0)) {
-          $data[$host]['status'] = 0;
-          $data[$host]['status_human'] = 'OK';
-          $data[$host]['status_style'] = 'ok';
-        } elseif (($data[$host]['hoststatus']['last_hard_state'] == 2) | ($data[$host]['servicestatus']['last_hard_state'] == 1)) {
-          $data[$host]['status'] = 1;
-          $data[$host]['status_human'] = 'WARNING / UNREACHABLE';
-          $data[$host]['status_style'] = 'warning';
-        } elseif (($data[$host]['hoststatus']['last_hard_state'] == 1) | ($data[$host]['servicestatus']['last_hard_state'] == 2)) {
-          $data[$host]['status'] = 2;
-          $data[$host]['status_human'] = 'CRITICAL / DOWN';
-          $data[$host]['status_style'] = 'critical';
-        } else {
-          $data[$host]['status'] = 3;
-          $data[$host]['status_human'] = 'UNKNOWN - NagMap bug - please report to maco@blava.net !';
-          $data[$host]['status_style'] = 'critical';
-        }
+               // ====== NorNet-specific service handling ===================
+               if ($parameterName == "check_command") {
+                  // ====== Handle site =====================================
+                  if (preg_match("/^MySiteCheck!/", $value)) {
+                     $value = preg_replace("/^MySiteCheck!/", "", $value);
+                     $args  = str_getcsv($value, ' ');
+                     echo $args;
+                  }
+
+                  // ====== Handle tunnel ===================================
+                  elseif (preg_match("/^MyTunnelCheck!/", $value)) {
+                     $value = preg_replace("/^MyTunnelCheck!/", "", $value);
+                     $args  = str_getcsv($value, ' ');
+                     $localHostName  = "";
+                     $remoteHostName = "";
+                     for ($i = 0; $i < count($args); $i++) {
+                        if ($args[$i] == "-L") {
+                           $localHostName = $args[$i + 1];
+                           $i++;
+                        }
+                        else if ($args[$i] == "-R") {
+                           $remoteHostName = $args[$i + 1];
+                           $i++;
+                        }
+                     }
+                     $status[$hostName][$serviceName]['local_host_name']  = $localHostName;
+                     $status[$hostName][$serviceName]['remote_host_name'] = $remoteHostName;
+                  }
+               }
+
+               $status[$hostName][$serviceName][$parameterName] = $value;
+            }
+         }
       }
-    }
-  }
-  return $data;
+   }
+
+   foreach ($status as $hostName => $hostEntry) {
+      foreach ($hostEntry as $serviceName => $serviceEntry) {
+         foreach ($serviceEntry as $parameterName => $parameterEntry) {
+            echo "<br />".$hostName.".".$serviceName.".".$parameterName." = ".$parameterEntry;
+         }
+      }
+   }
+
+  return $status;
 }
 
 echo "Test1\n";
 
-$s = getNorNetStatus();
-echo $s;
+$status = getNorNetStatus();
 
-echo "Test2\n";
+echo "<br/>Test2\n";
 
 ?>
 
