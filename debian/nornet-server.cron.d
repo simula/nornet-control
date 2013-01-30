2c4d6f3eaa5034509afc952b9c933ad1f6244e37
Thomas Dreibholz <dreibh@simula.no>
2013-01-30 15:19:19 +0100

    Watchdog updates.

--- debian/nornet-server.cron.d
+++ debian/nornet-server.cron.d
@@ -1,1 +1,1 @@
-*/4 * * * *                                root    /usr/bin/Watchdog /etc/nornet/watchdog-config >/var/log/nornet-watchdog 2>&1
+*/4 * * * *                                root    /usr/bin/Watchdog /etc/nornet/watchdog-config >>/var/log/nornet-watchdog 2>&1
