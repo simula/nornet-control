bb6acc69f6c8ddd7063fef1595336b9ac4463e0c
Thomas Dreibholz <dreibh@simula.no>
2013-01-30 14:10:37 +0100

    Watchdog configuration updates.

--- debian/nornet-tunnelbox.cron.d
+++ debian/nornet-tunnelbox.cron.d
@@ -1,1 +1,2 @@
 3,8,13,18,23,28,33,38,43,48,53,58 * * * *  root    test -x /etc/init.d/nornet-tunnelbox && /etc/init.d/nornet-tunnelbox check-and-configure
+*/4 * * * *                                root    /usr/bin/Watchdog /etc/nornet/watchdog-config >/var/log/nornet-watchdog 2>&1
