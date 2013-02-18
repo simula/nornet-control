bea515d31bda5c2f72a52cffdb7678d825fd708b
Thomas Dreibholz <dreibh@simula.no>
2013-02-18 16:21:39 +0100

    Crontab improvement for backups.

--- debian/nornet-management.cron.d
+++ debian/nornet-management.cron.d
@@ -1,1 +1,6 @@
-0 2 * * *  root  if [ -d /nfs/node ] ; then mkdir -p /nfs/node/backup && System-Backup /nfs/node/backup daily 7 10800 >>/var/log/nornet-backup.log 2>&1 ; fi
+# Daily backup between 2:00 and 5:00
+0 2 * * *  root  if [ -d /nfs/node ] ; then mkdir -p /nfs/node/backup && System-Backup /nfs/node/backup daily  7 10800 >>/var/log/nornet-backup.log 2>&1 ; fi
+# Weekly backup on Sunday between 5:00 and 7:00
+0 5 * * 0  root  if [ -d /nfs/node ] ; then mkdir -p /nfs/node/backup && System-Backup /nfs/node/backup weekly 7 10800 >>/var/log/nornet-backup.log 2>&1 ; fi
+# Monthly backup on first day of month between 23:00 and 2:00
+0 23 1 * *  root  if [ -d /nfs/node ] ; then mkdir -p /nfs/node/backup && System-Backup /nfs/node/backup monthly 7 10800 >>/var/log/nornet-backup.log 2>&1 ; fi
