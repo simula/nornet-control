81d93ec38c60caabbf5444b18aef82caa43e0136
Thomas Dreibholz <dreibh@simula.no>
2013-05-30 09:07:55 +0200

    Using Random-Sleep for cron jobs.

--- debian/nornet-monitor.cron.d
+++ debian/nornet-monitor.cron.d
@@ -1,1 +1,1 @@
-2,7,12,17,22,27,22,27,42,47,52,57 * * * *  root    test -x /etc/init.d/nornet-monitor && service nornet-monitor check-and-configure
+2,7,12,17,22,27,22,27,42,47,52,57 * * * *  root    test -x /etc/init.d/nornet-monitor && /usr/bin/Random-Sleep 0 150 -quiet && service nornet-monitor check-and-configure
