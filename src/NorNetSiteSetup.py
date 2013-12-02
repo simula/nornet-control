9e14ea308039355703edcbe8cbaebeeb9b6e7888
Thomas Dreibholz <dreibh@simula.no>
2013-12-02 16:18:43 +0100

    Added yum repository configuration.

--- src/NorNetSiteSetup.py
+++ src/NorNetSiteSetup.py
@@ -555,6 +555,52 @@ def makeNorNetNode(site, nodeNiceName, nodeNorNetIndex,
             #print file['conf_file_id'], file['dest'],file['source']
             #getPLCServer().DeleteConfFile(getPLCAuthentication(), file['conf_file_id'])
 
+      # ====== Add yum repositories =========================================
+      print "x1"
+      yumRepoSourceFile = codecs.open('nornet.repo', 'r', 'utf-8')
+      yumRepoSource = yumRepoSourceFile.read()
+      yumRepoSourceFile.close()
+
+      yumKeySourceFile = codecs.open('nornet.key', 'r', 'utf-8')
+      yumKeySource = yumKeySourceFile.read()
+      yumKeySourceFile.close()
+
+      print "x1"
+      yumRepoName = '/var/www/html/PlanetLabConf/nornet.repo'
+      try:
+         yumRepo = codecs.open(yumRepoName, 'w', 'utf-8')
+         yumRepo.write(yumRepoSource)
+         yumRepo.close()
+      except:
+         print('WARNING: Unable to write ' + yumRepoName)
+
+      yumKeyName = '/var/www/html/PlanetLabConf/nornet.key'
+      try:
+         yumKey = codecs.open(yumKeyName, 'w', 'utf-8')
+         yumKey.write(yumKeySource)
+         yumKey.close()
+      except:
+         print('WARNING: Unable to write ' + yumRepoName)
+
+      print "x3"
+      fileSource      = yumRepoName
+      fileDestination = '/etc/yum.repos.d/nornet.repo'
+      confFileID = _addOrUpdateConfFile({
+         'file_owner'        : u'root',
+         'postinstall_cmd'   : u'',
+         'error_cmd'         : u'',
+         'preinstall_cmd'    : u'',
+         'dest'              : fileDestination,
+         'ignore_cmd_errors' : False,
+         'enabled'           : True,
+         'file_permissions'  : u'644',
+         'source'            : fileSource,
+         'always_update'     : False,
+         'file_group'        : u'root'})
+      if getPLCServer().AddConfFileToNode(getPLCAuthentication(), confFileID, nodeID) != 1:
+         error('Unable to add repository configuration file to node ' + nodeHostName)
+
+
       # ====== Provide proxy configurations =================================
       try:
          os.mkdir('/var/www/html/PlanetLabConf/proxy/')
