10e5f1e9001f4add510ef83a3fca46829fbbd5f4
Thomas Dreibholz <dreibh@simula.no>
2014-08-12 18:33:26 +0200

    Changed tunnelbox update interval to 20 minutes.

--- debian/nornet-tunnelbox.cron.d
+++ debian/nornet-tunnelbox.cron.d
@@ -1,1 +1,1 @@
-3,8,13,18,23,28,33,38,43,48,53,58 * * * *  root    test -x /etc/init.d/nornet-tunnelbox && /usr/bin/Random-Sleep 0 150 -quiet && service nornet-tunnelbox check-and-configure
+8,28,48 * * * *  root    test -x /etc/init.d/nornet-tunnelbox && /usr/bin/Random-Sleep 0 150 -quiet && service nornet-tunnelbox check-and-configure
