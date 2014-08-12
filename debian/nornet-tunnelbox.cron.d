a06bc8d9aa7b03ea2a85114eb86bdd4fabbb6606
Thomas Dreibholz <dreibh@simula.no>
2014-08-12 19:24:38 +0200

    Cron configuration update for tunnelbox.

--- debian/nornet-tunnelbox.cron.d
+++ debian/nornet-tunnelbox.cron.d
@@ -1,1 +1,1 @@
-8,28,48 * * * *  root    test -x /etc/init.d/nornet-tunnelbox && /usr/bin/Random-Sleep 0 150 -quiet && service nornet-tunnelbox check-and-configure
+8,38 * * * *  root    test -x /etc/init.d/nornet-tunnelbox && /usr/bin/Random-Sleep 0 1200 -quiet && service nornet-tunnelbox check-and-configure
