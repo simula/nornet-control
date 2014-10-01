55ac114e2a40e4695758a07e76953efe0291e83b
Thomas Dreibholz <dreibh@simula.no>
2014-10-01 22:19:53 +0200

    Updated test script.

--- src/mptcp/plot-test1.R
+++ src/mptcp/plot-test1.R
@@ -2,6 +2,16 @@ source("plotter.R")
 
 data <- loadResults("passive.flow-ReceivedBytes.data.bz2")
 
-filter <- (data$FromSite == 9) & (data$ToSite == 2)
 
+
+filter <- (data$CMT == "mptcp") & (data$IPVersion == 4) & (data$FromProviderIndex == 1) & (data$ToProviderIndex == 1)
 d <- subset(data, filter)
+
+
+x <- d$FromSiteIndex
+y <- d$ToSiteIndex
+z <- d$passive.flow.ReceivedBytes / (1024 * 1024)
+
+scatterplot3d(x, y, z, highlight.3d=TRUE,
+ col.axis="blue", col.grid="lightblue",
+ main="scatterplot3d - 2", pch=20)
