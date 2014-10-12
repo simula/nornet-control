915b1188482168b3a3d6898e47daf5d16018c018
Thomas Dreibholz <dreibh@simula.no>
2014-10-12 14:54:41 +0200

    Updated example.

--- src/mptcp/plot-test1.R
+++ src/mptcp/plot-test1.R
@@ -2,7 +2,16 @@ library(scatterplot3d)
 source("plotter.R")
 
 
+
+onlyNegativeResults <- FALSE
+filterProtocol      <- 4
+# outputFile          <- "test1.pdf"
+outputFile          <- sprintf("Results-IPv%s-%s.pdf", filterProtocol, onlyNegativeResults)
+
+
 data <- loadResults("passive.flow-ReceivedBytes.data.bz2")
+filter <- TRUE
+d <- subset(data, filter)
 
 
 getColor <- function(originalGain, minPercent, maxPercent)
@@ -24,14 +33,11 @@ getColor <- function(originalGain, minPercent, maxPercent)
 }
 
 
-filter <- (data$IPVersion == 4)
-d <- subset(data, filter)
-
 xTitle <- "Source Endpoint"
 xSet   <- factor(d$FromProviderIndex * 256 + d$FromSiteIndex)
 yTitle <- "Destination Endpoint"
 ySet   <- factor(d$ToProviderIndex * 256 + d$ToSiteIndex)
-zTitle <- "Multi-Path Transport Gain Factor in Log Scale [1]"
+zTitle <- "MPTCP Gain in Log Scale [1]"
 
 
 cat("X-Legend:\n")
@@ -61,7 +67,7 @@ x <- c()
 y <- c()
 z <- c()
 f <- c()
-for(ipVersion in levels(factor(d$IPVersion))) {
+for(ipVersion in c( filterProtocol )) {
    xLevel <- 1
    for(xValue in levels(xSet)) {
       yLevel <- 1
@@ -76,31 +82,31 @@ for(ipVersion in levels(factor(d$IPVersion))) {
 
          if(!is.na(zValue) && (zValue > 0.0)) {
 
-#             if(zValue > 10) {
-#                zValue <- 10
-#             }
-
-            if(zValue < 0.50) {
-               fValue <- "black"
-               cat("!!!",xLevel,yLevel,zValue,mptcpMean,tcpMean,"\n")
-            }
-            else if(zValue < 0.80) {
+            if(zValue < 0.80) {
                fValue <- "gray"
-               cat("xxx",xLevel,yLevel,zValue,mptcpMean,tcpMean,"\n")
+               info   <- "xxx"
+               if(zValue < 0.50) {
+                  fValue <- "black"
+                  info   <- "!!!"
+               }
+               cat(sep="", info, "\tx=",xLevel,"\ty=",yLevel,"\t",
+                   "from=",mptcp$FromProviderIndex[1],"/",mptcp$FromSiteIndex[1], " -> ",
+                   "to=",mptcp$ToProviderIndex[1],"/",mptcp$ToSiteIndex[1], "\t",
+                   "\tz=",zValue,"\ttcp=",tcpMean,"\tmptcp=",mptcpMean,"\n")
             }
             else {
-               fValue <- getColor(100.0 * zValue, -200.0, 20000.0)
+               fValue <- getColor(100.0 * zValue, -200.0, 2000.0)
             }
 
-zValue <- log(zValue)
-if(zValue < 0) {
- cat('neg',zValue,"\n")
-}
+            zLogValue <- log(zValue)
 
-            x <- append(x, xLevel)
-            y <- append(y, yLevel)
-            z <- append(z, zValue)
-            f <- append(f, fValue)
+            
+            if((onlyNegativeResults == FALSE) || (zValue < 0.80)) {
+               x <- append(x, xLevel)
+               y <- append(y, yLevel)
+               z <- append(z, zLogValue)
+               f <- append(f, fValue)            
+            }
          }
 
          yLevel <- yLevel + 1
@@ -109,9 +115,21 @@ if(zValue < 0) {
    }
 }
 
-pdf("test1.pdf", width=15, height=10, family="Helvetica", pointsize=22)
+
+x <- append(x, 0)
+y <- append(y, 0)
+z <- append(z, NA)
+f <- append(f, NA)
+
+x <- append(x, 27)
+y <- append(y, 27)
+z <- append(z, NA)
+f <- append(f, NA)
+
+pdf(outputFile, width=15, height=10, family="Helvetica", pointsize=22)
 scatterplot3d(x, y, z, highlight.3d=FALSE, color=f,
  xlab=xTitle, ylab=yTitle, zlab=zTitle,
+ lab=c(14,14), lab.z=6,
  col.axis="blue", col.grid="lightblue", type="h",
- pch=20)
+ pch=16)
 dev.off()
