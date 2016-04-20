b7000f64aea4ade3bc0b3e16f89693c3da46822b
Priv.-Doz. Dr. Thomas Dreibholz <dreibh@iem.uni-due.de>
2016-04-20 10:36:27 +0200

    A fix.

--- debian/nornet-trace-service.cron.d
+++ debian/nornet-trace-service.cron.d
@@ -1,2 +1,2 @@
 22 * * * *   root   if [ -e /etc/nornet/hipercontracer-database-configuration ] ; then /usr/bin/Random-Sleep 0 3200 -quiet && flock -x -n /var/lock/nornet-trace-import.lock -c "/usr/bin/tracedataimporter /etc/nornet/hipercontracer-database-configuration >>/var/log/nornet-trace-import.log 2>&1" ; fi
-01 * * * *   root   service nornet-trace-service start >/dev/null 2>&1
+# --- 01 * * * *   root   service nornet-trace-service start >/dev/null 2>&1
