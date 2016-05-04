3401ebb9a9fe2ae80c94a26874b98ad827c253c2
Thomas Dreibholz <dreibh@simula.no>
2016-05-04 17:03:58 +0200

    Trace Service cron job improvement.

--- debian/nornet-trace-service.cron.d
+++ debian/nornet-trace-service.cron.d
@@ -1,1 +1,2 @@
-22 * * * *   root   if [ -e /etc/nornet/hipercontracer-database-configuration ] ; then /usr/bin/Random-Sleep 0 3200 -quiet && flock -x -n /var/lock/nornet-trace-import.lock -c "/usr/bin/tracedataimporter /etc/nornet/hipercontracer-database-configuration >>/var/log/nornet-trace-import.log 2>&1" ; fi
+# Run at 00:00 and 12:00, random start between 00:00 and 11:30 hours later.
+0 0,12 * * *   root   if [ -e /etc/nornet/hipercontracer-database-configuration ] ; then /usr/bin/Random-Sleep 0 41400 -quiet && flock -x -n /var/lock/nornet-trace-import.lock -c "/usr/bin/tracedataimporter /etc/nornet/hipercontracer-database-configuration >>/var/log/nornet-trace-import.log 2>&1" ; fi
